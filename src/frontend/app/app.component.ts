6f92a8f122ded4d563868b84ec0168a0a38c0100
Neil MacDougall <neil.macdougall@suse.com>
2018-02-12 08:56:57 +0000

    Improve popup menu display on desktop

--- src/frontend/app/app.component.ts
+++ src/frontend/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ElementRef, Renderer, HostBinding, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
@@ -7,7 +7,6 @@ import { LoggedInService } from './logged-in.service';
 import { create, PartialLogger } from 'rxjs-spy';
 import { environment } from '../environments/environment';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,10 +14,12 @@ import { environment } from '../environments/environment';
 })
 
 export class AppComponent implements OnInit, AfterContentInit {
+
   constructor(
     private store: Store<AppState>,
     private router: Router,
-    private loggedInService: LoggedInService
+    private loggedInService: LoggedInService,
+    private renderer: Renderer2,
   ) {
     if (!environment.production) {
       if (environment.showObsDebug || environment.disablePolling) {
@@ -38,6 +39,13 @@ export class AppComponent implements OnInit, AfterContentInit {
           spy.pause('poll');
         }
       }
+    }
+
+    // Reasonable effort at detecting desktop browser
+    const isTouchDevice = function() {  return 'ontouchstart' in window || 'onmsgesturechange' in window; };
+    const isDesktopBrowser = window.screenX != 0 && !isTouchDevice() && !window.orientation ? true : false;
+    if (isDesktopBrowser) {
+      this.renderer.addClass(document.body, 'md-desktop');
     }
 
   }
