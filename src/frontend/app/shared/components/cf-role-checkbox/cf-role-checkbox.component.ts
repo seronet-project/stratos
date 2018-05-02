8d09ac93c3bd942156e106a4ac2c332cb2aa84de
Richard Cox <richard.cox@suse.com>
2018-05-02 17:08:55 +0100

    Disable org user if org or spaces are checked/semi checked

--- src/frontend/app/shared/components/cf-role-checkbox/cf-role-checkbox.component.ts
+++ src/frontend/app/shared/components/cf-role-checkbox/cf-role-checkbox.component.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { combineLatest, filter, first } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 
+import { OrgUserRoleNames, SpaceUserRoleNames } from '../../../features/cloud-foundry/cf.helpers';
 import {
   CfRolesService,
   CfUserRolesSelected,
@@ -15,8 +16,7 @@ import {
   selectManageUsersPicked,
 } from '../../../store/actions/users.actions';
 import { AppState } from '../../../store/app-state';
-import { CfUser, IUserPermissionInOrg } from '../../../store/types/user.types';
-import { OrgUserRoleNames } from '../../../features/cloud-foundry/cf.helpers';
+import { CfUser, IUserPermissionInOrg, IUserPermissionInSpace } from '../../../store/types/user.types';
 
 
 
@@ -116,18 +116,102 @@ export class CfRoleCheckboxComponent implements OnInit, OnDestroy {
     return { checked, tooltip };
   }
 
+  private static hasSpaceRole(
+    users: CfUser[],
+    existingRoles: CfUserRolesSelected,
+    newRoles: IUserPermissionInOrg,
+    orgGuid: string,
+    spaces: { [guid: string]: IUserPermissionInSpace },
+    checkedSpaces: Set<string>
+  ): boolean {
+    const spaceGuids = Object.keys(spaces || {});
+    for (let y = 0; y < spaceGuids.length; y++) {
+      const spaceGuid = spaceGuids[y];
+      if (checkedSpaces.has(spaceGuid)) {
+        continue;
+      }
+      const manager = CfRoleCheckboxComponent.getCheckedState(SpaceUserRoleNames.MANAGER, users, existingRoles, newRoles, orgGuid, spaceGuid);
+      const developer = CfRoleCheckboxComponent.getCheckedState(SpaceUserRoleNames.DEVELOPER, users, existingRoles, newRoles, orgGuid, spaceGuid);
+      const auditor = CfRoleCheckboxComponent.getCheckedState(SpaceUserRoleNames.AUDITOR, users, existingRoles, newRoles, orgGuid, spaceGuid);
+      const hasSpaceRole = manager.checked !== false || developer.checked !== false || auditor.checked !== false;
+      if (hasSpaceRole) {
+        return true;
+      }
+      checkedSpaces.add(spaceGuid);
+    }
+  }
+
+  private static hasOrgSpaceRole(
+    users: CfUser[],
+    existingRoles: CfUserRolesSelected,
+    newRoles: IUserPermissionInOrg,
+    orgGuid: string): boolean {
+    if (!newRoles) {
+      return false;
+    }
+
+    // Check all org roles, if any existing or new is set then return true
+    const manager = CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.MANAGER, users, existingRoles, newRoles, orgGuid);
+    const billing = CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.BILLING_MANAGERS, users, existingRoles, newRoles, orgGuid);
+    const auditor = CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.AUDITOR, users, existingRoles, newRoles, orgGuid);
+    const hasOrgRole = manager.checked !== false || billing.checked !== false || auditor.checked !== false;
+    if (hasOrgRole) {
+      return true;
+    }
+
+    // Check all space roles
+    // .. first check new space roles
+    const checkedSpaces = new Set<string>();
+    if (CfRoleCheckboxComponent.hasSpaceRole(users, existingRoles, newRoles, orgGuid, newRoles.spaces, checkedSpaces)) {
+      return true;
+    }
+
+    // .. second check existing space roles
+    const existingUserGuids = Object.keys(existingRoles);
+    for (let x = 0; x < existingUserGuids.length; x++) {
+      const orgs = existingRoles[existingUserGuids[x]];
+      const org = orgs[orgGuid];
+      if (CfRoleCheckboxComponent.hasSpaceRole(users, existingRoles, newRoles, orgGuid, org.spaces, checkedSpaces)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+  /**
+   * Determine if the checkbox should be disabled. This is only relevant to the Org User role which should be disabled if there are any
+   * existing or new org or space roles set
+   *
+   * @private
+   * @static
+   * @param {boolean} isOrgRole
+   * @param {string} role
+   * @param {CfUser[]} users
+   * @param {CfUserRolesSelected} existingRoles
+   * @param {IUserPermissionInOrg} newRoles
+   * @param {string} orgGuid
+   * @param {Boolean} checked
+   * @returns {boolean} True if thie checkbox should be disabled
+   * @memberof CfRoleCheckboxComponent
+   */
   private static isDisabled(
     isOrgRole: boolean,
     role: string,
     users: CfUser[],
     existingRoles: CfUserRolesSelected,
     newRoles: IUserPermissionInOrg,
-    orgGuid: string): boolean {
-    // Determine if the checkbox is disabled (is this the org user checkbox and are other org roles true (true || null))
-    if (isOrgRole && role === 'user') {
-      return CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.MANAGER, users, existingRoles, newRoles, orgGuid).checked !== false ||
-        CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.BILLING_MANAGERS, users, existingRoles, newRoles, orgGuid).checked !== false ||
-        CfRoleCheckboxComponent.getCheckedState(OrgUserRoleNames.AUDITOR, users, existingRoles, newRoles, orgGuid).checked !== false;
+    orgGuid: string,
+    checked: Boolean): boolean {
+    if (isOrgRole && role === OrgUserRoleNames.USER) {
+      // Never disable the org user checkbox if it's not enabled/semi enabled (covers odd cases when cf creates orgs/spaces without the
+      // permissions)
+      const isChecked = checked === true || checked === null;
+      if (!isChecked) {
+        return false;
+      }
+
+      // Check all org and space roles, are there any set? If so we should disable the org user checkbox
+      return CfRoleCheckboxComponent.hasOrgSpaceRole(users, existingRoles, newRoles, orgGuid);
     }
     return false;
   }
@@ -147,7 +231,8 @@ export class CfRoleCheckboxComponent implements OnInit, OnDestroy {
         this.role, users, existingRoles, newRoles, this.orgGuid, this.spaceGuid);
       this.checked = checked;
       this.tooltip = tooltip;
-      this.disabled = CfRoleCheckboxComponent.isDisabled(this.isOrgRole, this.role, users, existingRoles, newRoles, this.orgGuid);
+      // this.disabled = this.isOrgRole && this.role === OrgUserRoleNames.USER && (checked === true || checked === null);
+      this.disabled = CfRoleCheckboxComponent.isDisabled(this.isOrgRole, this.role, users, existingRoles, newRoles, this.orgGuid, checked);
     });
   }
 
@@ -171,20 +256,10 @@ export class CfRoleCheckboxComponent implements OnInit, OnDestroy {
       }
       if (this.isOrgRole) {
         this.store.dispatch(new ManageUsersSetOrgRole(this.orgGuid, this.role, checked));
-        // if (checked && this.role !== 'user' && !newRoles.permissions.user) {
-        //   this.store.dispatch(new ManageUsersSetOrgRole(this.orgGuid, 'user', true));
-        // }
       } else {
         this.store.dispatch(new ManageUsersSetSpaceRole(this.orgGuid, this.spaceGuid, this.role, checked));
       }
-      this.safeChanged(checked);
     });
-  }
-
-  private safeChanged(checked) {
-    if (this.changed) {
-      // this.changed.next(checked);
-    }
   }
 
 }
